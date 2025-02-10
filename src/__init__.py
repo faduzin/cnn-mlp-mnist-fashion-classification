4af98e31df678c7c7c283d6c4eac9c2bb96a225b
faduzin <eric.fadul@gmail.com>
2025-02-10 17:11:30 -0300

    update

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from src.mlp import build_mlp
 from src.cnn import build_cnn, plot_first_images
-from src.utils import data_info, count_classes, plot_training_history, evaluate_model, plot_confusion_matrix
-from src.data_preprocessing import load_data, preprocess_data, save_data
+from src.utils import load_data, save_data, data_info, count_classes, plot_training_history, evaluate_model, plot_confusion_matrix
+from src.data_preprocessing import data_preprocess
 
 __all__ = [
     "build_mlp", 
@@ -14,5 +14,5 @@ __all__ = [
     "plot_confusion_matrix",
     "load_data",
     "save_data",
-    "preprocess_data"
+    "data_preprocess"
     ]
